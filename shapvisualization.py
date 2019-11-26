2e298caa66384f26d5f6e8aba22cc1921cc6846e
Dr. Daniel Müller <dm@spyglassventures.ch>
2019-11-26 07:50:24 +0100

    Add files via upload

---
+++ shapvisualization.py
@@ -0,0 +1,17 @@
+import xgboost
+import shap
+
+# load JS visualization code to notebook
+shap.initjs()
+
+# train XGBoost model
+X,y = shap.datasets.boston()
+model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
+
+# explain the model's predictions using SHAP values
+# (same syntax works for LightGBM, CatBoost, scikit-learn and spark models)
+explainer = shap.TreeExplainer(model)
+shap_values = explainer.shap_values(X)
+
+# visualize the first prediction's explanation (use matplotlib=True to avoid Javascript)
+shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:])
